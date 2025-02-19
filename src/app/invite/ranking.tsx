a8811467cbc7d6cbd219610a4f5549bd80fe5e52
Kaio Dantas Mancini <dantaskaio01@gmail.com>
2025-02-19 16:11:38 -0300

    aula 1 e 2

---
+++ src/app/invite/ranking.tsx
@@ -0,0 +1,60 @@
+import Image from 'next/image'
+
+import copper from '../../assets/medal-copper.svg'
+import gold from '../../assets/medal-gold.svg'
+import silver from '../../assets/medal-silver.svg'
+
+export function Ranking() {
+  return (
+    <div className="w-full max-w-[440px] space-y-5">
+      <h2 className="text-gray-200 text-xl font-heading font-semibold leading-none">
+        Ranking de Indicações
+      </h2>
+      <div className="space-y-4">
+        <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
+          <span className="text-sm text-gray-300 leading-none">
+            <span className="font-semibold">1º</span> | Kaio Mancini
+          </span>
+          <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
+            1050
+          </span>
+          <Image
+            src={gold}
+            alt="Medalha de ouro"
+            className="absolute right-8 top-0"
+          />
+        </div>
+      </div>
+      <div className="space-y-4">
+        <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
+          <span className="text-sm text-gray-300 leading-none">
+            <span className="font-semibold">2º</span> | Kaio Mancini
+          </span>
+          <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
+            1050
+          </span>
+          <Image
+            src={silver}
+            alt="Medalha de ouro"
+            className="absolute right-8 top-0"
+          />
+        </div>
+      </div>
+      <div className="space-y-4">
+        <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
+          <span className="text-sm text-gray-300 leading-none">
+            <span className="font-semibold">3º</span> | Kaio Mancini
+          </span>
+          <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
+            1050
+          </span>
+          <Image
+            src={copper}
+            alt="Medalha de ouro"
+            className="absolute right-8 top-0"
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
